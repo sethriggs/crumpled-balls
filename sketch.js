b45710c0eb0bfc7b442e523cc60f1ea552f159f0
sethriggs <71795367+sethriggs@users.noreply.github.com>
2020-11-30 13:53:02 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var dustBin, paper;
+var ground;
+
+//function preload()
+//{
+	
+//}
+
+function setup() {
+	createCanvas(800, 700);
+
+	rectMode(CENTER);
+  
+	engine = Engine.create();
+	world = engine.world;
+	Engine.run(engine);
+  
+	dustbin = new DustBin(720, 390, 100, 10);
+	paper = new Paper(200, 300, 10);
+	ground = Bodies.rectangle(width / 2, 400, width, 10,
+	{
+	  isStatic: true
+	});
+	World.add(world, ground);
+  
+	
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+
+  rectMode(CENTER);
+  background(0);
+  dustbin.display();
+  paper.display();
+
+  keyPressed();
+  drawSprites();
+ 
+}
+
+function keyPressed()
+{
+  if (keyCode === UP_ARROW ) {
+    Matter.Body.applyForce(paper.body, paper.body.position, {
+      x: 12,
+      y: -13
+    });
+  }
+}
+
